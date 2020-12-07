82af0c9985c6b00561231cc8f59f93b06048770d
n8kel <nate.kelley@digicert.com>
2020-12-07 12:19:26 -0700

    Updates

--- config/cabinCameraApi.ts
+++ config/cabinCameraApi.ts
@@ -1,3 +1,4 @@
 import axios from 'axios';
 
-export const getCabinImages = () => axios.get('https://boudfamilycabin.duckdns.org/files').then(({ data }) => data);
+export const getCabinImages = (number = 1) =>
+  axios.get(`https://boudfamilycabin.duckdns.org/files?number=${number}`).then(({ data }) => data);
--- pages/cabin.vue
+++ pages/cabin.vue
@@ -1,29 +1,53 @@
 <template>
   <div class="cabin">
     Cabin Page
-    {{ JSON.stringify(images) }}
+
+    <input type="number" v-model="imageNum" />
+
+    <div v-for="(image, index) in images" :key="index" class="images">
+      <img :src="datImages(image)" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop } from 'vue-property-decorator';
+import { Vue, Component, Prop, Watch } from 'vue-property-decorator';
 import { getCabinImages } from '@/config/cabinCameraApi';
 
 @Component({ components: {} })
 export default class CabinPage extends Vue {
   images = [];
+  imageNum = 1;
 
-  async getImages() {
-    const images = await getCabinImages();
-    console.log(images);
-
-    this.images = images;
+  datImages(imageObject: any) {
+    const url = 'https://boudfamilycabin.duckdns.org';
+    return url + imageObject.path;
   }
 
   created() {
     this.getImages();
   }
+
+  @Watch('imageNum')
+  async getImages() {
+    const images = await getCabinImages(this.imageNum);
+    this.images = images[0].images;
+    console.log(this.images);
+  }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.cabin {
+  margin-bottom: 150px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .images {
+    max-width: 70vw;
+    img {
+      width: 100%;
+    }
+  }
+}
+</style>
